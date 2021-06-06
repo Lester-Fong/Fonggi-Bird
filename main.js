908dd34dabe74a1d353171e5924e61d50e704b2b
Lester Niel <lesterfonggi123@gmail.com>
2021-06-06 16:55:18 +0800

    Initial Commit

---
+++ main.js
@@ -0,0 +1,89 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = 600;
+canvas.height = 400;
+
+let spacepressed = false;
+let angle = 0;
+let hue = 0;
+let frame = 0;
+let score = 0;
+let gamespeed = 2;
+
+const gradient = ctx.createLinearGradient(0, 0, 0, 70);
+gradient.addColorStop('0.4', '#ff0');
+gradient.addColorStop('0.5', '#fff');
+gradient.addColorStop('0.55', '#4040ff');
+gradient.addColorStop('0.6', '#fff');
+gradient.addColorStop('0.9', 'darkred');
+
+const background = new Image();
+background.src = '/imgs/BG.png';
+const BG = { 
+    x1: 0,
+    x2: canvas.width,
+    y: 0,
+    width: canvas.width,
+    height: canvas.height
+}
+
+function handleBackground() {
+    if (BG.x1 <= -BG.width + gamespeed) BG.x1 = BG.width;
+    else BG.x1 -= gamespeed;
+    if (BG.x2 <= -BG.width + gamespeed) BG.x2 = BG.width;
+    else (BG.x2 -= gamespeed)
+    ctx.drawImage(background, BG.x1, BG.y, BG.width, BG.height)
+    ctx.drawImage(background, BG.x2, BG.y, BG.width, BG.height)
+}
+
+function animate (){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+//  ctx.fillRect(10, canvas.height - 90, 50, 50);
+    handleBackground();
+    handleObstacles();
+    handleParticles(); 
+    bird.update();
+    bird.draw ();
+    ctx.fillStyle = gradient;
+    ctx.font = '90px Oswald'
+    ctx.strokeText(score, 450, 70);
+    ctx.fillText(score, 450, 70);
+    handleCollisions();
+    if (handleCollisions()) return;
+    requestAnimationFrame(animate);
+    angle+= 0.12;
+    hue++;
+    frame++;
+}
+animate ();
+
+window.addEventListener ('keydown', function (e) {
+    if (e.code === 'Space' ) spacepressed = true;
+});
+
+window.addEventListener ('keyup', function (e) {
+    if (e.code === 'Space' ) spacepressed = false;
+    bird.frameX = 0;
+});
+
+const bang = new Image ();
+bang.src = '/imgs/bang.png';
+function handleCollisions () {
+    for (let i = 0; i < obstaclesArray.length; i++) {
+        if (bird.x < obstaclesArray[i].x + obstaclesArray[i].width &&
+            bird.x + bird.width > obstaclesArray[i].x &&
+            ((bird.y < 0 + obstaclesArray[i].top && bird.y + bird.height > 0)
+            || (bird.y > canvas.height - obstaclesArray[i].bottom && bird.y
+                + bird.height < canvas.height))) {
+                    // collision detected
+
+                    ctx.drawImage(bang, bird.x, bird.y, 50, 50);
+                    ctx.font = '25px Oswald';
+                    ctx.fillStyle = 'white';
+                    ctx.fillText ('Game Over, your score is ' + score, 160, canvas.height/2 - 10);
+
+                    return true;
+                }
+    }
+}
+
---
+++ obstacles.js
@@ -0,0 +1,38 @@
+const obstaclesArray = [];
+
+class Obstacle {
+    constructor () {
+        this.top = (Math.random () * canvas.height/3) + 20;
+        this.bottom = (Math.random () * canvas.height/3) + 20;
+        this.x = canvas.width;
+        this.width = 40;
+        this.color = 'hsla(' + hue + ', 100%, 50%, .9)';
+        this.counted = false;
+    }
+
+    draw() {
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, 0, this.width, this.top);
+        ctx.fillRect(this.x, canvas.height - this.bottom, this.width, this.bottom);
+    }
+    update () {
+        this.x -= gamespeed;
+        if (!this.counted && this.x < bird.x) {
+            score++;
+            this.counted = true;
+        }
+        this.draw();
+    }
+}
+
+function handleObstacles() {
+    if (frame%70 ===0) {
+        obstaclesArray.unshift(new Obstacle);
+    }
+    for (let i = 0; i < obstaclesArray.length; i++) {
+        obstaclesArray[i].update();
+    }
+    if (obstaclesArray.length > 20) {
+        obstaclesArray.pop(obstaclesArray[0]);
+    }
+}
